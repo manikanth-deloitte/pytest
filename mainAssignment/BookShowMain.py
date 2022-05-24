60b2a2d865b05129eddc38d51f9a8daff6d4ae9b
Manikanth <amanikanth@deloitte.com>
2022-05-24 19:02:33 +0530

    admin login is added

---
+++ mainAssignment/BookShowMain.py
@@ -0,0 +1,45 @@
+# from adminClass import adminAction
+# from register import userRegister
+# from user import userlogin
+
+
+class Welcome:
+    def welcome(self):
+        print("******Welcome to BookMyShow*******\n1.Login\n2.Register\n3.Exit")
+
+
+def option():
+    option = int(input("enter option: "))
+    return option
+
+
+def main():
+    admin_dic = {
+        'pass': 'admin123'
+    }
+    # created obj of welcome class
+    wel = Welcome()
+    while True:
+        wel.welcome()
+        opt = option()
+
+        if opt == 1:
+            print("******Welcome to BookMyShow*******")
+            user = input("enter login id: ")
+            password = input("enter password:")
+            if user == 'Admin':
+                if admin_dic['pass'] == password:
+                    adminAction()
+            else:
+                print("******Welcome to BookMyShow******* ")
+                userlogin(user, password)
+
+        elif opt == 2:
+            print("****Create new Account***** ")
+            userRegister()
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
